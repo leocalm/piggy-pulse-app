a51efdd03d2006f4b87889a872f468c608f60d68
Leonardo Almeida <leocalm@gmail.com>
2026-02-04 17:19:42 +0100

    test: silence recharts warnings in storybook

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,6 +1,10 @@
 import { setProjectAnnotations } from '@storybook/react-vite';
+import { defaultResponsiveContainerProps } from 'recharts/es6/component/responsiveContainerUtils';
 import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 setProjectAnnotations([projectAnnotations]);
+
+// Avoid initial -1 sizing warnings from recharts ResponsiveContainer in storybook tests.
+defaultResponsiveContainerProps.initialDimension = { width: 1, height: 1 };
---
+++ src/types/recharts-shims.d.ts
@@ -0,0 +1,8 @@
+declare module 'recharts/es6/component/responsiveContainerUtils' {
+  export const defaultResponsiveContainerProps: {
+    initialDimension: {
+      width: number;
+      height: number;
+    };
+  };
+}
